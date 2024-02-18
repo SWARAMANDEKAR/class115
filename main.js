f302f9eafff3e0788f64e4efd296275def8cceda
SWARAMANDEKAR <143893341+SWARAMANDEKAR@users.noreply.github.com>
2024-02-18 20:43:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 https://teachablemachine.withgoogle.com/models/F-tQ5MssU/
+
+function startclassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/F-tQ5MssU/model.json',modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
